6e085453160f32c97ab2e9ee81e0bcaa2c7afa35
Youngsan <youngoxid@gmail.com>
2026-05-07 00:06:13 +0900

    .gitignore 파일 수정 및 누락 프로젝트들 추가

---
+++ W12D2_HeapAndStack/HeapAndStack.cs
@@ -0,0 +1,34 @@
+﻿namespace W12D2_HeapAndStack
+{
+    internal class HeapAndStack
+    {
+        struct SCardStat
+        {
+            public int Damage;
+            public int Cost;
+        }
+
+        class CardEffect
+        {
+            public int Damage;
+            public int Cost;
+        }
+
+        void ModifyStat(SCardStat stat)
+        {
+            stat.Damage = 999;
+        }
+
+        void ModifyEffect(CardEffect effect)
+        {
+            effect.Damage = 999;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
---
+++ W1to11_Test/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace W1to11_Test
+{
+    interface IAttackable
+    {
+        public void TakeDamage(int amount);
+    }
+
+    interface IBuffable
+    {
+        public void GetBuff(int amount);
+    }
+
+    class Player
+    {
+
+    }
+
+    class Monster
+    {
+
+    }
+
+    class Trap
+    {
+
+    }
+
+    class Chest
+    {
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
